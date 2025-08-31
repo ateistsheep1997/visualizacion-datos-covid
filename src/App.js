e73fdeadca0ffc7130c397b93f4ab4a96da20ca0
ateistsheep1997 <laguayonasu2fz@hotmail.com>
2025-08-31 16:53:28 +0200

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/about' component={About} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
