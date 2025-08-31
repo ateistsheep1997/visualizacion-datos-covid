9b8dc74c71e75ce1c4b47731478d4ef80e949643
ateistsheep1997 <laguayonasu2fz@hotmail.com>
2025-08-31 16:53:29 +0200

    feat: initial commit for src/components/Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Bienvenido a la Visualización de Datos COVID-19</h1>
+      <p>Aquí podrás encontrar datos actualizados sobre la pandemia.</p>
+    </div>
+  );
+}
+
+export default Home;
