b391294c509793db01e1b016ed8c552214ec01a1
ateistsheep1997 <laguayonasu2fz@hotmail.com>
2025-08-31 16:53:30 +0200

    feat: initial commit for src/components/About.js

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1>Sobre este Proyecto</h1>
+      <p>Este proyecto tiene como objetivo proporcionar una visualización informativa y atractiva de los datos de COVID-19.</p>
+    </div>
+  );
+}
+
+export default About;
